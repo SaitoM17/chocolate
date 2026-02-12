b66fb8442c3c7c52b1c21bd314b74d38f2d94e71
Said Mariano <smariano170@gmail.com>
2026-02-12 15:31:32 -0600

    feat: Se agrego consula para la obtención del total de las transacciones

--- sql/analisis_exploratorio_datos.sql
+++ sql/analisis_exploratorio_datos.sql
@@ -5,3 +5,11 @@ FROM
 	chocolatesales;
     
 -- Las ventas totales son $19,791,583
+
+### Total de transacciones
+SELECT
+	COUNT('Sales Person') AS cantidad
+FROM
+	chocolatesales;
+    
+-- Se han registrado 3282 transacciones desde 2022 hasta el 2025
